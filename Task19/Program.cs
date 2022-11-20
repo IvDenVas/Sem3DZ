c22a09168740f5f3a8b67dadc9ba5964bc0cf110
Denis <ivdenvas@gmail.com>
2022-11-20 12:39:36 +0300

    finish task19

---
+++ Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        if (number > 9999 && number <100000)
+            break;
+    Console.WriteLine("Ошибка ввода!");
+}
+MyMetod(number);
+static void MyMetod(int number)
+{
+    if((number / 10000) == (number % 10) && ((number / 1000) % 10) == (number / 10) % 10)
+        Console.WriteLine($"{number} -> да");
+    else
+        Console.WriteLine($"{number} -> нет");
+}

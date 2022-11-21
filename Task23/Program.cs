c345918ff533e2030c0b02acccb05aa3c579c78e
Denis <ivdenvas@gmail.com>
2022-11-21 11:54:32 +0300

    finish task23

---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿int num;
+while (true)
+{
+    Console.Write("Введите число N: ");
+    if (int.TryParse(Console.ReadLine(), out num))
+        if (num > 0)
+            break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int count = 1;
+int res = 0;
+MyMetod(num, count, res);
+static void MyMetod(int num, int count, int res)
+{
+   while (count <= num)
+   {
+        res = count * count * count;
+        if (count < num)
+            Console.Write($"{res}, ");
+        else
+            Console.Write($"{res}");
+        count++;
+    }
+}
